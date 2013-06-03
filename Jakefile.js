4a317a1d617fdabeb3c78a12b90ac4f655c0ff8b
Jared D. Cottrell <jared@objectlabs.com>
2013-06-03 16:22:18 -0700

    Applying copyright and license header; renaming LICENSE.txt -> LICENSE

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2013 ObjectLabs Corporation
- * Distributed under the MIT license - http://opensource.org/licenses/MIT
- */
-
 var mocha = require('./lib/jake-mocha');
 mocha.defineTask(
         {
